cab6f49d74f1997c8c84a0a4999693e012da9d05
Megasonic988 <weixiang@ualberta.ca>
2016-01-30 13:08:30 -0700

    first commit

---
+++ DoorManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DoorManager
+//
+//  Created by Kevin Wang on 2016-01-26.
+//  Copyright (c) 2016 Kevin Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DoorManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DoorManager
+//
+//  Created by Kevin Wang on 2016-01-26.
+//  Copyright (c) 2016 Kevin Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
